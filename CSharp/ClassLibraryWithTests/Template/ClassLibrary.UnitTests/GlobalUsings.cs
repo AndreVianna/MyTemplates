659b6456201c6baafedd5c986f7c4adf9e62618e
Andre Vianna <andre.vianna@tc.gc.ca>
2022-08-29 09:23:16 -0400

    Update template

--- CSharp/ClassLibraryWithTests/Template/ClassLibrary.UnitTests/GlobalUsings.cs
+++ CSharp/ClassLibraryWithTests/Template/ClassLibrary.UnitTests/GlobalUsings.cs
@@ -4,9 +4,9 @@ global using System.Diagnostics.CodeAnalysis;
 
 global using AutoBogus;
 
-global using ClassLibrary.Model;
-global using ClassLibrary.Services;
-global using ClassLibrary.TestUtilities;
+global using $ext_safeprojectname$.Model;
+global using $ext_safeprojectname$.Services;
+global using $ext_safeprojectname$.TestUtilities;
 
 global using FluentAssertions;
 
